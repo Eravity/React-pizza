d63b93a68d3804bfce9733fd544f5ef02238ad78
Cristian Cebotari <cebotaricristian92@gmail.com>
2024-03-14 16:58:32 +0200

    v1.1.0

--- src/assets/components/footer.tsx
+++ src/assets/components/footer.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import Order from "./orderButton";
 
 const Footer: React.FC = () => {
+  const [time, setTime] = useState(new Date());
+
+  // Current time function
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   const openHour = 12;
   const closingHour = 22;
+
+  // current time formatted
+  const formattedTime = time.toLocaleTimeString();
+
   return (
     <footer className="footer flex items-center flex-col gap-10">
       <p>
         We are open from {openHour}:00 to {closingHour}:00. Come visit us or
         order online.
       </p>
+      <p>Current time: {formattedTime}</p>
       <Order />
     </footer>
   );
